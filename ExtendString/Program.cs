f7c8bdc41a48be6ff4d9dc222a8889db0713968c
João Inácio <egnawake@gmail.com>
2022-11-29 10:49:16 -0100

    Complete exercise 1

---
+++ ExtendString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExtendString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine(args[0].ToRandomCase());
+        }
+    }
+}
